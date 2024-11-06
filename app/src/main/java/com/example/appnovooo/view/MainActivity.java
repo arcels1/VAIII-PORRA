badbb01f671ecc2bd3c25a5e78841de590405edc
arcels1 <viniciusalvesdelimacosta1@gmail.com>
2024-11-05 21:14:52 -0300

    limpar funciona mais ou menos mas taok

--- app/src/main/java/com/example/appnovooo/view/MainActivity.java
+++ app/src/main/java/com/example/appnovooo/view/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.appnovooo.view;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +13,11 @@ import model.Curso;
 import model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText nome;
+    EditText sobrenome;
+    EditText curso;
+    EditText telefone;
+    Button limpar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +28,43 @@ public class MainActivity extends AppCompatActivity {
         pessoa.setCurso(new Curso("Desenvolvimento de sistemas"));
         pessoa.setSobrenome("alves");
         pessoa.setTelefone("994564-4562");
+        listeners();
+        evento();
+        nome.setText(pessoa.getNome());
+        sobrenome.setText(pessoa.getSobrenome());
+        curso.setText(pessoa.getCurso().getNomeCurso());
+        telefone.setText(pessoa.getTelefone());
 
-        Pessoa outrapessoa = new Pessoa();
-        outrapessoa.setNome("Carlos");
-        outrapessoa.setCurso(new Curso("Pensador"));
-        outrapessoa.setSobrenome("Roberto");
-        outrapessoa.setTelefone("9148165");
-        Log.i("Pessoas:",pessoa.toString()+outrapessoa.toString());
-asda
+//        Pessoa outrapessoa = new Pessoa();
+//        outrapessoa.setNome("Carlos");
+//        outrapessoa.setCurso(new Curso("Pensador"));
+//        outrapessoa.setSobrenome("Roberto");
+//        outrapessoa.setTelefone("9148165");
+//        Log.i("Pessoas:",pessoa.toString()+outrapessoa.toString());
+
     int parada = 0;
 
     }
+
+    @SuppressLint("WrongViewCast")
+    public void limpar(){
+        nome.setText("Primeiro Nome");
+        sobrenome.setText("Sobrenome");
+        curso.setText("Nome do Curso Desejado");
+        telefone.setText("Telefone de Contato");
+    }
+
+    public void listeners(){
+        nome =(EditText) findViewById(R.id.primeironome);
+        sobrenome =(EditText) findViewById(R.id.sobrenome);
+        curso =(EditText) findViewById(R.id.curso);
+        telefone =(EditText) findViewById(R.id.telefone);
+        limpar = findViewById(R.id.limpar);
+
+    }
+    public void evento(){
+        limpar.setOnClickListener(evt ->limpar());
+    }
+
 }
 
