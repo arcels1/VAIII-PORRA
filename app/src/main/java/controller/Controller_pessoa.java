32cf146f5f3e3f69c06d6b2a00002f1bea304668
arcels1 <viniciusalvesdelimacosta1@gmail.com>
2024-11-19 19:53:11 -0300

    funcionando aprovado Marusam

--- app/src/main/java/controller/Controller_pessoa.java
+++ app/src/main/java/controller/Controller_pessoa.java
@@ -10,19 +10,37 @@ import android.widget.Toast;
 import model.Pessoa;
 
 public class Controller_pessoa {
-    SharedPreferences.Editor editor;
+
+
+    EditText nome;
+    EditText sobrenome;
+    EditText curso;
+    EditText telefone;
+
+    Controller_pessoa controller;
+
+    public static final String NOME_PREFERENCES = "pref_listavip";
     Context contexto;
     SharedPreferences listaVIP;
+    SharedPreferences.Editor editor;
 
-    public Controller_pessoa(SharedPreferences.Editor editor, Context contexto, SharedPreferences listaVIP) {
-        this.editor = editor;
-        this.contexto = contexto;
-        this.listaVIP = listaVIP;
+
+    public Controller_pessoa(EditText nome,EditText sobrenome, EditText curso, EditText telefone) {
+
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.curso = curso;
+        this.telefone = telefone;
+        this.contexto = nome.getContext();
+
     }
 
     //testar inicio
     @SuppressLint("SetTextI18n")
-    public void iniciar(EditText nome,EditText sobrenome, EditText curso, EditText telefone){
+    public void iniciar(){
+        listaVIP = contexto.getSharedPreferences(NOME_PREFERENCES,Context.MODE_PRIVATE);
+        editor = listaVIP.edit();
+
         if(listaVIP.contains("Primeiro nome")){
            nome.setText(listaVIP.getString("Primeiro nome",""));
         }
@@ -39,9 +57,11 @@ public class Controller_pessoa {
     }
 
     public void salve(Pessoa pessoa) {
+        if(!error(pessoa)) {
             adicionarPreferencias(pessoa);
-            Log.i("MVC CONTROLLER","Pessoa Salva "+pessoa.toString());
+            Log.i("MVC CONTROLLER", "Pessoa Salva " + pessoa.toString());
             Toast.makeText(contexto, "Salvo com sucesso", Toast.LENGTH_SHORT).show();
+        }
         }
 
     public void finalize(){
@@ -52,11 +72,13 @@ public class Controller_pessoa {
         }
 
 
-    public void limpe(EditText nome, EditText sobrenome, EditText curso, EditText telefone){
+    public void limpe(){
         nome.setText("");
         sobrenome.setText("");
         curso.setText("");
         telefone.setText("");
+        editor.clear();
+        editor.commit();
 
     }
 
@@ -68,6 +90,31 @@ public class Controller_pessoa {
         editor.commit();
 
 
+
+    }
+
+    public boolean error(Pessoa pessoa){
+        String nomeTeste = nome.getText().toString().trim();
+        String sobrenomeTeste = sobrenome.getText().toString().trim();
+        String cursoTeste = curso.getText().toString().trim();
+        String telefoneTeste = telefone.getText().toString().trim();
+
+        boolean vazio = false;
+        if ( nomeTeste.isEmpty()){
+            vazio=true;
+            nome.setError("Campo obrigatório");
+        } else if (sobrenomeTeste.isEmpty()) {
+            vazio=true;
+            sobrenome.setError("Campo obrigatório");
+        } else if (cursoTeste.isEmpty()) {
+            vazio=true;
+            curso.setError("Campo obrigatório");
+        } else if (telefoneTeste.isEmpty()) {
+            vazio=true;
+            telefone.setError("Campo obrigatório");
+        }
+        return vazio;
+        //salva
     }
 
 
